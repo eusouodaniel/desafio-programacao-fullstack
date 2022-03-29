8bc777cfb58877c9d6f912d6f40139b5f844c097
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 12:16:38 -0300

    NEW: Import all services in transaction util

--- src/utils/transaction.util.ts
+++ src/utils/transaction.util.ts
@@ -1,5 +1,15 @@
 import IProductFields from '../interfaces/IProductFields';
 
+import AffiliateService from '../services/AffiliateService';
+import ProductService from '../services/ProductService';
+import TransactionService from '../services/TransactionService';
+import TransactionTypeService from '../services/TransactionTypeService';
+
+const affiliateService = new AffiliateService();
+const productService = new ProductService();
+const transactionService = new TransactionService();
+const transactionTypeService = new TransactionTypeService();
+
 class TransactionUtil {
 
   public async processImportTransaction(transactionFields: IProductFields[]) {
