bdc930c57c00fa99e164cdd99b9f4d8cef92d135
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 12:24:32 -0300

    NEW: Add logic to hash pass

--- src/entities/user.model.ts
+++ src/entities/user.model.ts
@@ -6,6 +6,8 @@ import {
   UpdateDateColumn
 } from "typeorm";
 
+import bcrypt from "bcryptjs";
+
 @Entity("users")
 class User {
 
@@ -32,6 +34,14 @@ class User {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+
+  checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+    return bcrypt.compareSync(unencryptedPassword, this.password);
+  }
 
 }
 
