7f3e80d7e4a3ce2225098ebc54ae7ddc6aa2dd4d
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 12:29:55 -0300

    NEW: Create routes transactions authenticated

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -3,13 +3,15 @@ import multer from 'multer';
 
 import uploadConfig from '../config/upload';
 import TransactionController from '../controllers/transaction.controller';
+import AuthJWT from "../middlewares/auth.jwt";
+import AuthRole from "../middlewares/auth.role";
 
 const transactionRouter = Router();
 const transactionController = new TransactionController();
 
 const upload = multer(uploadConfig);
 
-transactionRouter.get('/', transactionController.index);
-transactionRouter.post('/import', upload.single('file'), transactionController.import);
+transactionRouter.get('/', [AuthJWT, AuthRole(["PRODUCER"])], transactionController.index);
+transactionRouter.post('/import', [AuthJWT, AuthRole(["PRODUCER"])], upload.single('file'), transactionController.import);
 
 export default transactionRouter;
