434742816e26e650e8c9e145e4423998ffc13cd7
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 12:36:46 -0300

    NEW: Create user by test

--- src/__tests__/useCases/transaction.spec.ts
+++ src/__tests__/useCases/transaction.spec.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 import app from '../../app';
 import { AppDataSource } from '../../database';
+import User from '../../entities/user.model';
 
 describe('Transaction', () => {
   let token = "";
@@ -30,5 +31,24 @@ describe('Transaction', () => {
     await AppDataSource.manager.query('DROP TABLE IF EXISTS typeorm_metadata');
 
     await AppDataSource.destroy();
+  });
+
+  it('should be able to create user', async () => {
+    const userRepository = AppDataSource.getRepository(User);
+
+    let user = new User();
+    user.email = "producer-test";
+    user.password = "producer-test";
+    user.hashPassword();
+    user.role = "PRODUCER";
+
+    const saveUser = await userRepository.save(user);
+
+    expect(saveUser).toEqual(
+      expect.objectContaining({
+        email: 'producer-test',
+        role: 'PRODUCER',
+      })
+    );
   });
 });
