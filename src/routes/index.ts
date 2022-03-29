42ece99a581b1b8a4d0f50edf1c8e85d30cb0827
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 12:31:02 -0300

    NEW: Add swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import swaggerUi from "swagger-ui-express";
 
 import healthRouter from './health.routes';
 import transactionRouter from './transaction.routes';
+import swaggerFile from "../swagger.json";
 
 const routes = Router();
 
+routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 routes.use('/healthz', healthRouter);
 routes.use('/transactions', transactionRouter);
 
