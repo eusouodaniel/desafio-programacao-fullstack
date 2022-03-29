c9c5cd5c4fce8306c1444d4740ce4f53b8d26210
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 12:02:54 -0300

    NEW: Add file in endpoint

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import TransactionController from '../controllers/transaction.controller';
 
 const transactionRouter = Router();
 const transactionController = new TransactionController();
 
+const upload = multer(uploadConfig);
+
 transactionRouter.get('/', transactionController.index);
-transactionRouter.get('/import', transactionController.import);
+transactionRouter.get('/import', upload.single('file'), transactionController.import);
 
 export default transactionRouter;
