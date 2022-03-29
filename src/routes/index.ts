a5077c20feaf79278d2e2251893515d583c90419
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 12:32:02 -0300

    NEW: Add routes to login and get info by user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import swaggerUi from "swagger-ui-express";
 
+import authRouter from './auth.routes';
 import healthRouter from './health.routes';
 import transactionRouter from './transaction.routes';
 import swaggerFile from "../swagger.json";
@@ -8,6 +9,7 @@ import swaggerFile from "../swagger.json";
 const routes = Router();
 
 routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+routes.use('/auth', authRouter);
 routes.use('/healthz', healthRouter);
 routes.use('/transactions', transactionRouter);
 
