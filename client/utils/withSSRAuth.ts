5863f369f68f15f2100530a0ad02b30f20b78af0
eusouodaniel <danielrodriguesdrs331@gmail.com>
2022-03-29 21:03:30 -0300

    NEW: Create utils ssr

---
+++ client/utils/withSSRAuth.ts
@@ -0,0 +1,59 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import { destroyCookie, parseCookies } from "nookies";
+import decode from 'jwt-decode'
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type WithSSRAuthOptions = {
+  roles?: string[];
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions) {
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    const token = cookies['nextauth.token'];
+
+    if (!token) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        }
+      }
+    }
+
+    if (options) {
+      const user = decode<{ roles: string[] }>(token);
+      const { roles } = options
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        roles 
+      })
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false,
+          }
+        }
+      }
+    }
+
+    try {
+      return await fn(ctx)
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token')
+    
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          }
+        }
+      }
+    }
+  }
+}
